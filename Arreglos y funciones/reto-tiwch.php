bc53d8e4fe03b27ed1dd6400027577bc92d5fe44
JAFETH JESUS SANCHEZ SANCHEZ <jafeth.sanchez@senado.gob.mx>
2024-08-05 11:32:28 -0600

    Reto de if completado

--- Arreglos y funciones/reto-tiwch.php
+++ Arreglos y funciones/reto-tiwch.php
@@ -5,7 +5,7 @@ $dinero = readline("Ingrese la cantidad de dinero que desea retirar: ");
 
 if ($dinero >= 100){
     echo "Retirando tu dinero";
-    } 
+} 
 else{
     echo "No se puede retirar esa cantidad, tienes que tener mas de 100 dolares";
-    }
+}
