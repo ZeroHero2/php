5f95fec4fefa083317f75a30ad1895f57b1b7a50
JAFETH JESUS SANCHEZ SANCHEZ <jafeth.sanchez@senado.gob.mx>
2024-08-05 09:58:10 -0600

    Reto de Michis completado

--- ejercicio-michis.php
+++ ejercicio-michis.php
@@ -1,3 +1,37 @@
 <?php
 // Ejercicio de michis 
 // 3 michis nombre, ocupacion, color y comida subdivididas en comidas favoritas y las que no les gustan
+$michis = [
+    $Alberto = [
+        'nombre' => 'Alberto',
+        'ocupacion' => 'Dormir',
+        'color' => 'Blanco',
+        'comida' => [
+            'favorita' => 'Pescado',
+            'no_gusta' => 'Verduras'
+        ]
+    ],
+
+    $Ramses = [
+        'nombre'=> 'Ramses',
+        'ocupacion'=> 'Pelear',
+        'color' => 'Blanco/negro',
+        'comida' => [
+            'favorita' => 'salmon',
+            'no_gusta' => 'pimiento' 
+        ]
+    ],
+
+    $Rogelio = [
+        'nombre'=> 'Rogelio',
+        'ocupacion'=> 'jugar',
+        'color' => 'Dorado',
+        'comida' => [
+            'favorita' => 'carne',
+            'no_gusta' => 'croquetas' 
+        ]
+    ],
+
+];
+
+echo "La comida favorita de Rogelio es: {$michis[2]['comida']['favorita']}";
