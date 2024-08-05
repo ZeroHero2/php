29cf728e4f98f759a80ebba426cd6e87782b6292
JAFETH JESUS SANCHEZ SANCHEZ <jafeth.sanchez@senado.gob.mx>
2024-08-04 23:52:39 -0600

    cambio de carpeta en archivos php

--- Php/arreglos.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo ("Hola");
-// Arreglos 
-$edades = [18,40,20];
-echo "Una de las edades de mi arreglo es: " . $edades[1];
-$edades2 = array(19,20,56);
-echo "Otra de las edades es" . $edades2[1];
