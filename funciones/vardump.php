060129e8b5a8ed8975e9836d5fdf7f98578c3da3
Jafeth Jesus <78237469+ZeroHero2@users.noreply.github.com>
2024-10-20 18:45:40 -0600

    Carpeta para funciones

---
+++ funciones/vardump.php
@@ -0,0 +1,18 @@
+<?php
+$texto = 'Hola';
+$numero = 54;
+$numero2 = 32.1;
+$arreglo = ["Carlos","Laura","Mario","Jose"];
+$arreglo_asociativo = ['nombre'=> "Carlos",'edad'=>15];
+$booleano = true;
+
+var_dump($texto);
+echo '</br>';
+var_dump($arreglo);
+echo '</br>';
+
+echo '------- Funcion printr -------';
+echo '</br>';
+print_r($numero);
+echo '</br>';
+print_r($arreglo);
